5a32ff83a2c434d7e29684be7f8e556705542583
Ju Qiang/鞠强 <840046209@qq.com>
2013-09-11 20:26:41 +0800

    add monitor

---
+++ fortbf/src/org/leepood/monitordemo/bootReceiver.java
@@ -0,0 +1,34 @@
+package org.leepood.monitordemo;
+
+import java.util.HashMap;
+import java.util.Iterator;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+public class bootReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		// 判断是否是开机发出的广播
+		Log.i("action", intent.getAction());
+		if (intent.getAction() == Intent.ACTION_BOOT_COMPLETED) {
+
+		} else if (intent.getAction() == monitorApp.ACTION) {
+			// Log.i("action", "received");
+			Bundle bundle = intent.getBundleExtra("bundle");
+			HashMap hashmap = (HashMap) bundle.getSerializable("app_info");
+			Iterator<String> it = hashmap.keySet().iterator();
+			while (it.hasNext()) {
+				String key=it.next();
+				String value=hashmap.get(key).toString();
+				Log.i("hashmap", key+":"+value);
+			}
+		}
+
+	}
+
+}
---
+++ fortbf/src/org/leepood/monitordemo/main.java
@@ -0,0 +1,17 @@
+package org.leepood.monitordemo;
+
+import org.jq.nbz.R;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class main extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        this.startService(new Intent(this,monitorApp.class));
+    }
+}
