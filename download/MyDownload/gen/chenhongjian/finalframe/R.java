02376f32b7361a5670190f5180b9b4996a042616
Ju Qiang/鞠强 <840046209@qq.com>
2013-09-11 20:45:55 +0800

    down demo

--- download/MyDownload/gen/chenhongjian/finalframe/R.java
+++ download/MyDownload/gen/chenhongjian/finalframe/R.java
@@ -4,107 +4,60 @@
  * aapt tool from the resource data it found.  It
  * should not be modified by hand.
  */
-
 package chenhongjian.finalframe;
 
 public final class R {
-    public static final class attr {
-    }
-    public static final class color {
-        public static final int TextColor=0x7f070000;
-        public static final int TitleTextColor=0x7f070001;
-    }
-    public static final class drawable {
-        public static final int button_background=0x7f020000;
-        public static final int button_bg_retry=0x7f020001;
-        public static final int delete_movie_bg=0x7f020002;
-        public static final int ic_action_search=0x7f020003;
-        public static final int ic_launcher=0x7f020004;
-        public static final int listview_line=0x7f020005;
-        public static final int mediacontroller_bg=0x7f020006;
-        public static final int mediacontroller_pause01=0x7f020007;
-        public static final int mediacontroller_pause02=0x7f020008;
-        public static final int mediacontroller_pause_button=0x7f020009;
-        public static final int mediacontroller_play01=0x7f02000a;
-        public static final int mediacontroller_play02=0x7f02000b;
-        public static final int mediacontroller_play_button=0x7f02000c;
-        public static final int mediacontroller_seekbar=0x7f02000d;
-        public static final int mediacontroller_seekbar01=0x7f02000e;
-        public static final int mediacontroller_seekbar02=0x7f02000f;
-        public static final int mediacontroller_seekbar_thumb=0x7f020010;
-        public static final int movie_headimage_selector=0x7f020011;
-        public static final int no=0x7f020012;
-        public static final int ok=0x7f020013;
-        public static final int progress_bg=0x7f020014;
-        public static final int start=0x7f020015;
-        public static final int stop=0x7f020016;
-        public static final int test_head_image=0x7f020017;
-    }
-    public static final class id {
-        public static final int bottom=0x7f090008;
-        public static final int current_progress=0x7f090012;
-        public static final int delete_movie=0x7f09000b;
-        public static final int download_bt=0x7f090015;
-        public static final int download_listview=0x7f090004;
-        public static final int download_listview_lin=0x7f09001c;
-        public static final int download_manager_clear=0x7f090003;
-        public static final int download_manager_edit=0x7f090002;
-        public static final int download_name=0x7f090016;
-        public static final int download_progressBar=0x7f09000e;
-        public static final int downloadmanager=0x7f09000a;
-        public static final int imageView1=0x7f090014;
-        public static final int index=0x7f090009;
-        public static final int listview=0x7f090006;
-        public static final int mediacontroller_file_name=0x7f09001b;
-        public static final int mediacontroller_play_pause=0x7f090017;
-        public static final int mediacontroller_seekbar=0x7f09001a;
-        public static final int mediacontroller_time_current=0x7f090018;
-        public static final int mediacontroller_time_total=0x7f090019;
-        public static final int menu_settings=0x7f09001e;
-        public static final int movie_file_size=0x7f090010;
-        public static final int movie_headiamge_checkbox=0x7f09000f;
-        public static final int movie_headimage=0x7f09000c;
-        public static final int movie_name_item=0x7f09000d;
-        public static final int relativeLayout1=0x7f090001;
-        public static final int stop_download_bt=0x7f090011;
-        public static final int surface_view=0x7f09001d;
-        public static final int textView1=0x7f090005;
-        public static final int title=0x7f090000;
-        public static final int totalsize=0x7f090013;
-        public static final int viewPager=0x7f090007;
-    }
-    public static final class layout {
-        public static final int activity_download_manager=0x7f030000;
-        public static final int activity_main=0x7f030001;
-        public static final int index_activity=0x7f030002;
-        public static final int list_download_item=0x7f030003;
-        public static final int list_main_item=0x7f030004;
-        public static final int mediacontroller=0x7f030005;
-        public static final int new_activity_download_manager=0x7f030006;
-        public static final int videoview=0x7f030007;
-    }
-    public static final class menu {
-        public static final int activity_main=0x7f080000;
-    }
-    public static final class raw {
-        public static final int libarm=0x7f040000;
-    }
-    public static final class string {
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
-        public static final int mediacontroller_play_pause=0x7f05000a;
-        public static final int menu_settings=0x7f050002;
-        public static final int title_activity_main=0x7f050003;
-        public static final int vitamio_init_decoders=0x7f050005;
-        public static final int vitamio_name=0x7f050004;
-        public static final int vitamio_videoview_error_button=0x7f050009;
-        public static final int vitamio_videoview_error_text_invalid_progressive_playback=0x7f050007;
-        public static final int vitamio_videoview_error_text_unknown=0x7f050008;
-        public static final int vitamio_videoview_error_title=0x7f050006;
-    }
-    public static final class style {
-        public static final int AppTheme=0x7f060000;
-        public static final int MediaController_SeekBar=0x7f060001;
-        public static final int MediaController_Text=0x7f060002;
-    }
+	public static final class raw {
+		public static final int libarm = 0x7f040000;
+	}
+	public static final class id {
+		public static final int mediacontroller_time_current = 0x7f090018;
+		public static final int mediacontroller_file_name = 0x7f09001b;
+		public static final int mediacontroller_play_pause = 0x7f090017;
+		public static final int mediacontroller_seekbar = 0x7f09001a;
+		public static final int menu_settings = 0x7f09001e;
+		public static final int surface_view = 0x7f09001d;
+		public static final int mediacontroller_time_total = 0x7f090019;
+	}
+	public static final class style {
+		public static final int MediaController_Text = 0x7f060002;
+		public static final int AppTheme = 0x7f060000;
+		public static final int MediaController_SeekBar = 0x7f060001;
+	}
+	public static final class string {
+		public static final int vitamio_videoview_error_text_invalid_progressive_playback = 0x7f050007;
+		public static final int vitamio_videoview_error_button = 0x7f050009;
+		public static final int mediacontroller_play_pause = 0x7f05000a;
+		public static final int menu_settings = 0x7f050002;
+		public static final int vitamio_videoview_error_text_unknown = 0x7f050008;
+		public static final int vitamio_videoview_error_title = 0x7f050006;
+		public static final int vitamio_init_decoders = 0x7f050005;
+		public static final int title_activity_main = 0x7f050003;
+		public static final int vitamio_name = 0x7f050004;
+		public static final int app_name = 0x7f050000;
+		public static final int hello_world = 0x7f050001;
+	}
+	public static final class menu {
+		public static final int activity_main = 0x7f080000;
+	}
+	public static final class layout {
+		public static final int videoview = 0x7f030007;
+		public static final int activity_main = 0x7f030001;
+		public static final int mediacontroller = 0x7f030005;
+	}
+	public static final class drawable {
+		public static final int mediacontroller_pause_button = 0x7f020009;
+		public static final int mediacontroller_bg = 0x7f020006;
+		public static final int mediacontroller_pause02 = 0x7f020008;
+		public static final int mediacontroller_pause01 = 0x7f020007;
+		public static final int mediacontroller_play_button = 0x7f02000c;
+		public static final int mediacontroller_seekbar_thumb = 0x7f020010;
+		public static final int mediacontroller_play02 = 0x7f02000b;
+		public static final int mediacontroller_seekbar01 = 0x7f02000e;
+		public static final int mediacontroller_play01 = 0x7f02000a;
+		public static final int mediacontroller_seekbar = 0x7f02000d;
+		public static final int mediacontroller_seekbar02 = 0x7f02000f;
+		public static final int ic_launcher = 0x7f020004;
+		public static final int ic_action_search = 0x7f020003;
+	}
 }
